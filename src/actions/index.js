eb435d6db92c4fd19c427f1c605dc4220ff0c31d
Anya <anya.p.nguyen@gmail.com>
2018-04-13 10:51:04 -0700

    init and created skeleton files for redux

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const make_guess = () => ({
+    type: MAKE_GUESS
+})
+
+export const NEW_GAME = 'NEW_GAME';
+export const new_game = () => ({
+    type: NEW_GAME
+})
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux';
+
+export default createStore(
+    
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
